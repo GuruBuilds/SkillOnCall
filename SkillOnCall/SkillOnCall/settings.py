31b6c5f3016d60f19220a23c18daeef64c357070
DELL <shubhpatel.1048@gmail.com>
2025-05-11 13:32:39 +0530

    make some UI imp

--- SkillOnCall/SkillOnCall/settings.py
+++ SkillOnCall/SkillOnCall/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -105,6 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
