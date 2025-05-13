3676f107f4488110ca89c9c452a0e89622449d64
Gurpreet Singh <gurupreetsingh9073@gmail.com>
2025-05-13 23:17:57 +0530

    make access token and status working

--- SkillOnCall/booking/urls.py
+++ SkillOnCall/booking/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('view-bookings/', views.view_bookings, name='view_bookings'),
     path('my-allocation/', views.my_allocation, name='my_allocation'),
     # path('<int:booking_id>/', views.booking_detail, name='booking_detail'),
-    path('decline-booking/<int:booking_id>/', views.decline_booking, name='decline_booking'),
-    path('accept-booking/<int:booking_id>/', views.accept_booking, name='accept_booking'),
+    path('accept-booking/<str:access_token>/', views.accept_booking, name='accept_booking'),
+    path('decline-booking/<str:access_token>/', views.decline_booking, name='decline_booking'),
     # path('<int:booking_id>/complete/', views.complete_booking, name='complete_booking'),
 ]
